c9508a724c9871ccbdac96bb39ae0575ceda71b5
Getsuga743 <ivan_maier@hotmail.com>
2022-01-07 12:34:50 -0300

    add:(config) eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'plugin:prettier/recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
+    'plugin:jsx-a11y/recommended',,
+    'airbnb',
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    'react'
+    'react',
   ],
   rules: {
-    identation:4,
-  }
-}
+  },
+};
