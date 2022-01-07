fe359c993846e59a8d8f1eb1f56b004feff48237
Getsuga743 <ivan_maier@hotmail.com>
2022-01-07 12:35:22 -0300

    add: LayoutContainer

---
+++ src/layout/LayoutContainer.js
@@ -0,0 +1,9 @@
+import styled  from '@emotion/styled';
+
+export const LayoutContainer = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    max-width:1280px;
+    margin:auto;
+`
