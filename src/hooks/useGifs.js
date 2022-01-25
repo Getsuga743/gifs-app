47ccb5da926293b74344fe8eacc8e356dfafaf97
ivanmaierg <ivan_maier@hotmail.com>
2022-01-24 23:51:46 -0300

    feat:(useGifs) replace state with context

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,16 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import GifsContext from '../context/GifsContext';
 import { getGifs } from '../services/getGifs';
 
 const INITIAL_PAGE = 0;
 
 export default function useGif({ keyword } = { keyword: null }) {
   const keywordToUse = keyword || localStorage.getItem('LastKeyword');
-  const [gifs, setGifs] = useState([]);
 
   const [loading, setLoading] = useState(true);
   const [loadingNextPage, setLoadingNextPage] = useState(false);
 
   const [page, setPage] = useState(INITIAL_PAGE);
+  const { gifs, setGifs } = useContext(GifsContext);
 
   useEffect(() => {
     setLoading(true);
